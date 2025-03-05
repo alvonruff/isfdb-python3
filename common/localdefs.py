1cc777b27c3912c363ea76415dbdf9f4bba66ad6
Al von Ruff <alvonruff@gmail.com>
2025-03-05 15:54:26 -0500

    Mechanism to switch between Python2 and Python3

---
+++ common/localdefs.py
@@ -0,0 +1,26 @@
+#
+#     (C) COPYRIGHT 2005-2024   Al von Ruff, Bill Longley and Ahasuerus
+#       ALL RIGHTS RESERVED
+#
+#     The copyright notice above does not evidence any actual or
+#     intended publication of such source code.
+#
+#     Version: $Revision$
+#     Date: $Date$
+
+
+DBASE      	= "isfdb"
+DBASEHOST  	= "localhost"
+DO_ANALYTICS	= 0
+HTFAKE	   	= "/www.isfdb.org/cgi-bin"
+HTMLHOST   	= "www.isfdb.org"
+HTMLHOST_ALT   	= "isfdb.org"
+HTMLLOC	   	= "www.isfdb.org"
+LEGACY_PASSWORDS= 0
+#PASSWORD   	= "XXXXXX"
+PASSWORD   	= ""
+PROTOCOL        = "https"
+USERNAME   	= "root"
+UNICODE		= "iso-8859-1"
+WIKILOC         = "www.isfdb.org/wiki"
+WIKILOC_ALT     = "isfdb.org/wiki"
